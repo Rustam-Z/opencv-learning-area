d0f29fbef217ccf6cb3421f96bc79e9651522a59
Rustam-Z <zokirovrustam202@gmail.com>
2021-01-20 13:48:49 +0500

    updated

--- part3_resize_crop.py
+++ part3_resize_crop.py
@@ -7,7 +7,7 @@ print(img.shape)
 img_resize = cv2.resize(img, (200, 200))
 
 # Crop the image
-img_cropped = img[0:200, 200:500]  # y:y+h, x:x+w
+img_cropped = img[0:200, 200:600]  # y:y+h, x:x+w
 
 cv2.imshow("Image", img)
 cv2.imshow("Resized Image", img_resize)
